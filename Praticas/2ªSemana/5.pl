6383d2f5abe9004503b67adfda049748b15673f9
Xavier-Pisco <x.pisco@gmail.com>
2020-10-07 10:14:55 +0100

    Finished 2nd week exercises

---
+++ Praticas/2ªSemana/5.pl
@@ -0,0 +1,14 @@
+e_primo(2).
+e_primo(3).
+e_primo(X):-
+    X > 3,
+    integer(X),
+    X mod 2 =\= 0,
+    \+extra(X,3).
+extra(X,Y):-
+    X mod Y =:= 0.
+extra(X,Y):-
+    Y * Y < X,
+    Y1 is Y + 2,
+    extra(X,Y1).
+    
