6c7faa830a992b78e3dc2a6dba6dc54668a9253a
Xavier-Pisco <x.pisco@gmail.com>
2020-11-18 10:05:25 +0000

    Couldn't do 5th one the right way

---
+++ Praticas/MT1M/5.pl
@@ -0,0 +1,44 @@
+%participant(Id,Age,Performance)
+participant(1234, 17, 'Pé coxinho').
+participant(3423, 21, 'Programar com os pés').
+participant(3788, 20, 'Sing a Bit').
+participant(4865, 22, 'Pontes de esparguete').
+participant(8937, 19, 'Pontes de pen-drives').
+participant(2564, 20, 'Moodle hack').
+
+%performance(Id,Times)
+performance(1234,[120,120,120,120]).
+performance(3423,[32,120,45,120]).
+performance(3788,[110,2,6,43]).
+performance(4865,[120,120,110,120]).
+performance(8937,[97,101,105,110]).
+
+% Não sei criar uma lista com os participantes
+allPerfs:-
+	showList([1234, 3423, 3788, 4865, 8937]).
+
+showList([]).
+
+showList([PerformedHead | PerformedTail]):-
+	showParticipant(PerformedHead),
+	nl,
+	showList(PerformedTail).
+
+showParticipant(Particpant):-
+	write(Particpant),
+	write(':'),
+	participant(Particpant, _, Name),
+	write(Name),
+	write(':'),
+	performance(Particpant, Times),
+	write('['),
+	writeTimes(Times),
+	write(']').
+
+writeTimes([TimesHead | []]):-
+	write(TimesHead).
+
+writeTimes([TimesHead | TimesTail]):-
+	write(TimesHead),
+	write(','),
+	writeTimes(TimesTail).
