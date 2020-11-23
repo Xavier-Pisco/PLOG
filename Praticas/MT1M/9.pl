10e14cbaeb409c68b72d7ebaf57fd0d105b638c2
Xavier-Pisco <x.pisco@gmail.com>
2020-11-23 15:26:03 +0000

    Finished 9th exercise

---
+++ Praticas/MT1M/9.pl
@@ -0,0 +1,35 @@
+%participant(Id,Age,Performance)
+participant(1234, 17, 'Pé coxinho').
+participant(3423, 21, 'Programar com os pés').
+participant(3788, 20, 'Sing a Bit').
+participant(4865, 22, 'Pontes de esparguete').
+participant(8937, 19, 'Pontes de pen-drives').
+participant(2564, 20, 'Moodle hack').
+
+%performance(Id,Times)
+performance(1234,[120,120,120,120]).
+performance(3423,[32,120,45,120]).
+performance(3788,[110,2,6,43]).
+performance(4865,[120,120,110,120]).
+performance(8937,[97,101,105,110]).
+
+predX(Q,[R|Rs],[P|Ps]) :-
+    participant(R,I,P), I=<Q, !,
+    predX(Q,Rs,Ps).
+predX(Q,[R|Rs],Ps) :-
+    participant(R,I,_), I>Q,
+    predX(Q,Rs,Ps).
+predX(_,[],[]).
+
+/**
+predX(+Idade, +Participantes, -Performance).
+
+Este predicado recebe uma idade e uma lista de participantes e cria
+uma lista com os nomes das performances desses participantes que tenham uma
+idade menor ou igual à idade.
+
+O cut utilizado é um cut verde pois não altera o funcionamento do programa
+e a sua única funcionalidade é melhorar a eficácia do programa.
+
+
+*/
