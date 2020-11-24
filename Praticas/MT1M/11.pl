43f2a9177c7fa9044b6bff7e9c4f4d0fe4d15f39
Xavier-Pisco <x.pisco@gmail.com>
2020-11-24 17:07:45 +0000

    Copied 11th exercise

--- Praticas/MT1M/11.pl
+++ Praticas/MT1M/11.pl
@@ -7,16 +7,10 @@ langford(N,L):-
 	N > 0,
 	N2 is 2 * N,
 	length(L, N2),
+	impoeTodos(N, L).
+
+impoeTodos(0, _).
+impoeTodos(N, L):-
 	impoe(N, L),
-	removeHead(L, L1),
-	removeLast(L1, L2),
 	N1 is N - 1,
-	langford(N1, L2).
-
-removeHead([Head | Tail], Tail).
-removeLast([Head | Tail], [Head | []]):-
-	length(Tail, 1).
-
-removeLast([Head | Tail], [Head | NewTail]):-
-	\+length(Tail, 0),
-	removeLast(Tail, NewTail).
+	impoeTodos(N1, L).
