655dd8af249c2f02b7acc124f33a81da4dd1442f
Xavier-Pisco <x.pisco@gmail.com>
2020-11-24 23:41:55 +0000

    Fixed database

--- Praticas/MT12018/database.pl
+++ Praticas/MT12018/database.pl
@@ -4,10 +4,10 @@ flight('TP1923', 'LPPR', 'LPPT', 1115, 55, 'TAP').
 flight('TP1968', 'LPPT', 'LPPR', 2235, 55, 'TAP').
 flight('TP842', 'LPPT', 'LIRF', 1450, 195, 'TAP').
 flight('TP843', 'LIRF', 'LPPT', 1935, 195, 'TAP').
-flight('FR5483', 'LPPR', 'LEMD', 630, 105, 'TAP').
-flight('FR5484', 'LEMD', 'LPPR', 1935, 105, 'TAP').
-flight('AF1024', 'LFPG', 'LPPT', 940, 155, 'TAP').
-flight('AF1025', 'LPPT', 'LFPG', 1310, 155, 'TAP').
+flight('FR5483', 'LPPR', 'LEMD', 630, 105, 'RYR').
+flight('FR5484', 'LEMD', 'LPPR', 1935, 105, 'RYR').
+flight('AF1024', 'LFPG', 'LPPT', 940, 155, 'AFR').
+flight('AF1025', 'LPPT', 'LFPG', 1310, 155, 'AFR').
 
 %airport(Name, ICAO, Country)
 airport('Aeroporto Francisco Sá Carneiro', 'LPPR', 'Portugal').
