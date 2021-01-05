0a4c4a944734a313bce13b8e68002c643a8c03b7
Xavier-Pisco <x.pisco@gmail.com>
2021-01-05 13:01:39 +0000

    Finished 2018 test

--- Exercícios/Teste2/2018/resolucao.pl
+++ Exercícios/Teste2/2018/resolucao.pl
@@ -46,15 +46,19 @@ gym_pairs(ManHeights, WomenHeights, Delta, Pairs):-
 	all_distinct(Pair),
 	setHeights(ManHeights, WomenHeights, Delta, Pair, 1),
 	labeling([], Pair),
-	prepare(Pair, Pairs, 1).
+	prepare(Pair, Pairs, 1). % Just change output from a single list to a list with pairs
 
 setHeights(_, _, _, [], _).
 setHeights(ManHeights, WomenHeights, Delta, [PH | PT], Current):-
 	nth1(Current, ManHeights, ManHeigth),
-	nth1(X, WomenHeights, WomenHeight),
+	nth1(X, WomenHeights, WomenHeight), % Tries any womenHeight for the curren manHeight
 	0 =< ManHeigth - WomenHeight,
 	Delta >= ManHeigth - WomenHeight,
-	PH #= X,
+	PH #= X,	% If heights are compatible then the pair number is the number of the women
+	Next is Current + 1,
+	setHeights(ManHeights, WomenHeights, Delta, PT, Next).
+setHeights(ManHeights, WomenHeights, Delta, [PH | PT], Current):-
+	PH #= 0,
 	Next is Current + 1,
 	setHeights(ManHeights, WomenHeights, Delta, PT, Next).
 
@@ -63,3 +67,29 @@ prepare([PH | PT], [PSH | PST], Current):-
 	PSH = Current-PH,
 	Next is Current + 1,
 	prepare(PT, PST, Next).
+
+optimal_skating_pairs(ManHeights, WomenHeights, Delta, Pairs):-
+	length(ManHeights, Men),
+	length(WomenHeights, Women),
+	length(Pair, Men),
+	domain(Pair, 0, Women),
+	all_distinct_except_0(Pair),
+	setHeights(ManHeights, WomenHeights, Delta, Pair, 1),
+	countNonZeros(Pair, Number),
+	labeling([maximize(Number)], Pair),
+	prepare2(Pair, Pairs, 1). % Just change output from a single list to a list with pairs
+
+countNonZeros([], 0).
+countNonZeros([PH | PT], Number):-
+	countZeros(PT, Temp),
+	(PH is 0, Number is Temp; Number is Temp + 1).
+
+prepare2([],[],_).
+prepare2([0 | PT], Pairs, Current):-
+	Next is Current + 1,
+	prepare2(PT, Pairs, Next).
+prepare2([PH | PT], [PSH | PST], Current):-
+	PH \= 0,
+	PSH = Current-PH,
+	Next is Current + 1,
+	prepare2(PT, PST, Next).
