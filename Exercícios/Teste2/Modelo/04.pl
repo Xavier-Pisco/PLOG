25fbbc58a64a4a1157cae0ac7e65911ddc8fd91d
Xavier-Pisco <x.pisco@gmail.com>
2021-01-05 16:23:55 +0000

    simplified ex4

--- Exercícios/Teste2/Modelo/04.pl
+++ Exercícios/Teste2/Modelo/04.pl
@@ -1,41 +1,17 @@
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
 
+
 constroi(NEmb, Orcamento, EmbPorObjeto, CustoPorObjeto, EmbUsadas, Objetos):-
-	findall(Emb-Obj, temp(NEmb, Orcamento, EmbPorObjeto, CustoPorObjeto, Emb, Obj), Results), !,
-	findMax(Results, EmbUsadas, Objetos).
-
-temp(NEmb, Orcamento, EmbPorObjeto, CustoPorObjeto, EmbUsadas, Objetos):-
-	length(Objetos, 4),
 	length(EmbPorObjeto, N),
-	domain(Objetos, 1, N),
-	domain([EmbUsadas], 0, NEmb),
-	all_distinct(Objetos),
-	custoLimite(Orcamento, CustoPorObjeto, Objetos),
-	embalagensLimite(EmbPorObjeto, Objetos, EmbUsadas),
+
+	length(Temp, N),
+	domain(Temp, 0, 1),
+
+	count(1, Temp, #=, 4),
+	scalar_product(CustoPorObjeto, Temp, #=<, Orcamento),
+	scalar_product(EmbPorObjeto, Temp, #=, EmbUsadas),
 	EmbUsadas #=< NEmb,
-	labeling([], [EmbUsadas]),
-	labeling([], Objetos).
 
-findMax([], 0, []).
-findMax([Emb-Obj | Tail], MaxEmb, MaxObj):-
-	findMax(Tail, TempMax, TempObj),
-	(TempMax > Emb,
-		MaxEmb is TempMax,
-		MaxObj = TempObj;
-		MaxEmb is Emb,
-		MaxObj = Obj
-	).
-
-custoLimite(Orcamento, _, []):- Orcamento >= 0.
-custoLimite(Orcamento, CustoPorObjeto, [OH | OT]):-
-	Orcamento >= 0,
-	nth1(OH, CustoPorObjeto, Custo),
-	Orcamento1 is Orcamento - Custo,
-	custoLimite(Orcamento1, CustoPorObjeto, OT).
-
-embalagensLimite(_, [], 0).
-embalagensLimite(EmbPorObjeto, [OH | OT], EmbUsadas):-
-	nth1(OH, EmbPorObjeto, Emb),
-	embalagensLimite(EmbPorObjeto, OT, Temp),
-	EmbUsadas is Temp + Emb.
+	labeling([maximize(EmbUsadas)], Temp),
+	findall(Index, nth1(Index, Temp, 1), Objetos).
