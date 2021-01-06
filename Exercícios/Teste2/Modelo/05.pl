1ecc49c421b61c00ba8965fa71e125696ee0a470
Xavier-Pisco <up201806134@fe.up.pt>
2021-01-06 12:50:50 +0000

    Changed exercise 5 to cumulatives

--- Exercícios/Teste2/Modelo/05.pl
+++ Exercícios/Teste2/Modelo/05.pl
@@ -1,6 +1,7 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
+/*
 embrulha(Rolos, Presentes, RolosSelecionados):-
 	length(Rolos, N1),
 	length(Presentes, N2),
@@ -26,3 +27,34 @@ verify([]).
 verify([RH | RT]):-
 	RH >= 0,
 	verify(RT).
+*/
+
+embrulha(Rolos, Presentes, RolosSelecionados):-
+	length(Rolos, N1),
+	length(Presentes, N2),
+	length(RolosSelecionados, N2),
+	domain(RolosSelecionados, 1, N1),
+
+    getTasks(Presentes, RolosSelecionados, Tasks),
+    getMachines(Rolos, Machines, 1),
+    cumulatives(Tasks, Machines, [bound(upper)]),  
+    /* 
+    Verifica para toas as Tasks se Fim = Início + Duração.
+    Verifica se para todas as tasks com o mesmo Id (neste caso com o mesmo RoloSelecionado) se
+    a máquina com esse Id (neste caso o Rolo) cumpre o limite imposto.
+    O limite pode ser upper ou lower, no caso de ser upper significa que a máquina tem limite suficiente para todas,
+    no caso de ser lower significa que as tasks utilizam mais do que a máquina tem.
+    */
+    labeling([], RolosSelecionados).
+
+
+getTasks([],[],[]).
+getTasks([P | Presentes], [R | RolosSelecionados], [T | Tasks]):-
+    T = task(0, P, P, P, R), % task define-se através de task(Início, Duração, Fim, Consumo, Id Máquina)
+    getTasks(Presentes, RolosSelecionados, Tasks).
+
+getMachines([], [], _).
+getMachines([R | Rolos], [M | Machines], Id):-
+    M = machine(Id, R),  % Machine define-se através de machine(Id Máquina, Limite recurso)
+    Next is Id + 1,
+    getMachines(Rolos, Machines, Next).
